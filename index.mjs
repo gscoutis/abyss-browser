346c74be502cc5cda6797ebd48506ed2605f4f9a
notAperson535 <notAperson939@gmail.com>
2022-11-22 16:11:17 -0500

    fix bare-server-ndoe

--- index.mjs
+++ index.mjs
@@ -1,31 +1,57 @@
-import Server from 'bare-server-node';
-import http from 'http';
-import nodeStatic from 'node-static';
-import cluster from 'cluster';
-import os from 'os';
-import * as dotenv from 'dotenv'
-dotenv.config()
-const numCPUs = process.env.MAXCPUS;
-if(cluster.isMaster){
-console.log("Running");
-	for(let i = 0; i < numCPUs; i++){
-	cluster.fork();
-	}
-}else{
-const bare =  new Server('/bare/', '');
-const serve = new nodeStatic.Server('public/');
+import createBareServer from '@tomphttp/bare-server-node';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import serveStatic from 'serve-static';
 
-const server = http.createServer();
-server.on('request', (request, response) => {
-	
-    if (bare.route_request(request, response)){ 
-		//console.log(request.rawHeaders[request.rawHeaders.indexOf('userKey')+1])
-		return true;}
-    serve.serve(request, response);
+const httpServer = createServer();
+
+// Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
+const bareServer = createBareServer('/bare/', {
+	logErrors: false,
+	localAddress: undefined,
+	maintainer: {
+		email: 'tomphttp@sys32.dev',
+		website: 'https://github.com/tomphttp/',
+	},
 });
-server.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+
+// The static root is usually relative to the main script in projects that use the Bare server.
+// ie. if static.js is at /src/static.js, public will be /public/
+// ideally, you will point the public directory relative to the current working directory
+// serveStatic('./public/')
+// This would ignore the relative location of static.js
+const serve = serveStatic(
+	fileURLToPath(new URL('../public/', import.meta.url)),
+	{
+		fallthrough: false,
+	}
+);
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		serve(req, res, (err) => {
+			res.writeHead(err?.statusCode || 500, {
+				'Content-Type': 'text/plain',
+			});
+			res.end(err?.stack);
+		});
+	}
 });
-server.listen(process.env.PORT || 80, () => {console.log("Abyss running at http://localhost:80")});
-}
+
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+httpServer.on('listening', () => {
+	console.log('HTTP server listening');
+});
+
+httpServer.listen({
+	port: 8080,
+});
