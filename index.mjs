35d0f5de735f1198ef8500f8daf5264c01199a2a
Nick <55303069+830008@users.noreply.github.com>
2022-12-18 19:47:51 -0600

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,18 +2,11 @@ import createBareServer from '@tomphttp/bare-server-node';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import serveStatic from 'serve-static';
-import cluster from 'cluster';
 import * as dotenv from 'dotenv'
 dotenv.config()
 const httpServer = createServer();
 
-const numCPUs = process.env.MAXCPUS;
-if(cluster.isMaster){
-console.log("Running");
-	for(let i = 0; i < numCPUs; i++){
-	cluster.fork();
-	}
-}else{
+
 // Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
 const bareServer = createBareServer('/bare/', {
 	logErrors: false,
@@ -59,4 +52,4 @@ httpServer.on('listening', () => {
 httpServer.listen({
 	port: 8080,
 });
-}
+
